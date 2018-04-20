7fb08025b389b6c9da2a8863717ebcb0bfdc73e5
Megat Ilham <31387021+fhostpz@users.noreply.github.com>
2018-04-20 12:11:31 +0800

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<!-- <link href ="bootstrap.css" rel="stylesheet"> -->
+		<!-- <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script> -->
+		
+		<!-- <script src="jquery.js"></script> -->
+		<!-- <script src="bootstrap-treeview.js"></script> -->
+		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+		<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
+		
+		<!-- Bootstrap 4 -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+		<meta charset = "utf-8">
+		<meta name = "viewport" content = "width=device-width, initial-scale=1">
+		<title>Locations</title>
+		<link rel="stylesheet" href="background.css">
+	</head>
+	
+	<body>
+		<div class = "jumbotron jumbotron-fluid">
+			<div class="container">
+				<h1 class="display-4"> Cyber Security Progress Monitoring</h1>
+				<p class="lead"> </p>
+			</div>
+		</div>
+		<!-- Navbar -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light mt-0 pt-0 noTop noBack">
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			  </button>
+			<div class="collapse navbar-collapse" id="navbarText">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="Location.html">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Location</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Summary</a>
+				</li>
+				
+			</ul>
+			</div>
+		</nav>
+		<!-- Navbar End -->
+		<!-- Sidebar -->
+		<div class = "container-fluid position-relative ">
+			<div class ="row" id ="sidebar">
+				<div class="col-md-2 float-left col-1 pl-0 pr-0 collapse width show" id="sidebar">
+					<br>
+					<div class="list-group border-0 card text-center text-md-left">
+					
+					<a href="#menu1" class="list-group-item d-inline-block collapsed" data-toggle="collapse" aria-expanded="false">
+						<i class="fa fa-dashboard"></i> 
+						<span class="d-none d-md-inline">Sime Darby Plantation</span> 
+					</a>
+					
+					<div class="collapse" id="menu1" data-parent="#sidebar">
+						<a href="#menu1sub1" class="list-group-item" data-toggle="collapse" aria-expanded="false">Identify</a>
+						<div class="collapse" id="menu1sub1" data-parent="#menu1">
+							<a href="controls/Asset Management.html" class="list-group-item" data-parent="#menu1sub1">Asset Management</a>
+							<a href="controls/Business Environment.html" class="list-group-item" data-parent="#menu1sub1">Business Environment</a>
+							<a href="controls/Governance.html" class="list-group-item" data-parent="#menu1sub1">Governance</a>
+							<a href="controls/Risk Assessment.html" class="list-group-item" data-parent="#menu1sub1">Risk Assessment</a>
+							<a href="controls/Risk Management Strategy.html" class="list-group-item" data-parent="#menu1sub1">Risk Management Strategy </a>
+						</div>
+						
+						<a href="#menu1sub2" class="list-group-item" data-toggle="collapse" aria-expanded="false">Protect</a>
+							<div class="collapse" id="menu1sub2" data-parent="#menu1">
+								<a href="#" class="list-group-item" data-parent="#menu1sub2">Access Control</a>
+								<a href="#" class="list-group-item" data-parent="#menu1sub1">Awareness Training</a>
+								<a href="#menu1sub1sub1" class="list-group-item" data-toggle="collapse" aria-expanded="false">Data Security</a>
+									<div class="collapse" id="menu1sub1sub1">
+										<a href="#" class="list-group-item" data-parent="#menu1sub1sub1">Control</a>
+									</div>
+								<a href="#" class="list-group-item" data-parent="#menu1sub1">Information Protection Processes and Procedures</a> 
+								<a href="#menu1sub1sub2" class="list-group-item" data-toggle="collapse" aria-expanded="false">Maintenance </a>
+								<div class="collapse" id="menu1sub1sub2">
+									<a href="#" class="list-group-item">Control</a>
+								</div>
+							</div>
+
+					</div>
+				</div>
+			</div>
+			<!-- Sidebar End -->
+			<div class = "col-md-10 float-left col px-5 pl-md-2 pt-2">
+				<hr>
+				<div class="row">
+					<div class="col">
+						<b>Locations</b>
+					</div>
+					<div class="col">
+						<b>Percentage Done</b>
+					</div>
+					<div class="col">
+						<b>Last Updated</b>
+					</div>
+					
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col">
+						<a href="Domains.html" class="btn btn-primary btn-lg" role="button" aria-disabled="true">Sime Darby Plantation</a>
+					</div>
+					<div class="col">
+						<p>50%</p>
+					</div>
+					<div class="col">
+						<p>25/2/2018</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col">
+						<a href="#" class="btn btn-primary btn-lg" role="button" aria-disabled="true">H&K </a>
+					</div>
+					<div class="col">
+						<p>30%</p>
+					</div>
+					<div class="col">
+						<p>15/2/2018</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col">
+						<a href="#" class="btn btn-primary btn-lg" role="button" aria-disabled="true">Minamas </a>
+					</div>
+					<div class="col">
+						<p>30%</p>
+					</div>
+					<div class="col">
+						<p>15/2/2018</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col">
+						<a href="#" class="btn btn-primary btn-lg" role="button" aria-disabled="true">Unimills</a>
+					</div>
+					<div class="col">
+						<p>30%</p>
+					</div>
+					<div class="col">
+						<p>15/2/2018</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col">
+						<a href="#" class="btn btn-primary btn-lg" role="button" aria-disabled="true">Morakot </a>
+					</div>
+					<div class="col">
+						<p>30%</p>
+					</div>
+					<div class="col">
+						<p>15/2/2018</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col">
+						<a href="#" class="btn btn-primary btn-lg" role="button" aria-disabled="true">NBPOL </a>
+					</div>
+					<div class="col">
+						<p>30%</p>
+					</div>
+					<div class="col">
+						<p>15/2/2018</p>
+					</div>
+				</div>
+					
+			</div>
+		</div>
+	</body>
+
+
+</html>
